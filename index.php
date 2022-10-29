f52ecb21cce717efc27169979f63274140427969
yanoharuka <105197913+yanoharuka@users.noreply.github.com>
2022-10-29 14:55:55 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="utf-8" />
+
+    <meta name="viewport" content="width=device-width">
+        <title>PM</title>
+    </head>
+
+    <body>
+
+
+      <span class="nav_toggle">
+        <i></i>
+        <i></i>
+        <i></i>
+      </span>
+
+      <header>
+
+        <div class="pc">
+        <nav>
+          <ul class="main-menu">
+          <li><a href="/">About</a></li>
+          <li><a href="/item/">Program</a></li>
+          <li><a href="/about/">Live Streaming</a></li>
+          <li><a href="/contact/">Credit</a></li>
+          </ul>
+       </nav>
+        </div>
+
+        <div class="sp">
+        <div class="hamburger-menu">
+        <input type="checkbox" id="menu-btn-check">
+        <label for="menu-btn-check" class="menu-btn"><span></span></label>
+        <!--ここからメニュー-->
+        <div class="menu-content">
+            <ul>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Program</a>
+                </li>
+                <li>
+                    <a href="#">arcive</a>
+                </li>
+                <li>
+                  <a href="#">Credit</a>
+              </li>
+            </ul>
+        </div>
+        <!--ここまでメニュー-->
+    </div>
+        </div>
+
+      </header>
+
+      <img src="images/PMweb-1.png">
+    
+        <div class="box">
+
+         <div class="pc">
+          <img class=logoimg src="images/PM-iconPC.png">
+        </div>
+
+        <div class="sp">
+          <img class="logoimg" src="images/PM-iconSP.png">
+        </div>
+        
+
+            <img class="logo" src="" alt="写真">
+
+            <div class="About">
+
+            <h1 class="about">About</h1>
+            <p>
+              東京都立大学大学祭、『みやこ祭』を彩るプロジェクションマッピング。
+              <br>
+              インダストリアルアート学科の学生の有志企画で、今年で６回目の投影となりました。
+              <br>
+              例年は外壁に映していましたが、今回は初の室内投影に挑戦。
+              外では夜しか見ることのできなかったPMですが今年は室内で迫力のある映像をお届けします！
+              <br>
+
+            </p>
+            </div>
+
+            <h2>
+              日時
+            </h2>
+
+            <p>
+              11/3(木)、11/5(土)
+              <br>
+               東京都立大学みやこ祭１日目、３日目
+            </p>
+
+            <h2>
+              場所
+            </h2>
+
+            <p>
+              1号館1階103教室
+
+            </p>
+
+            <div class="Program">
+            
+            <h1>Program</h1>
+           
+            <p>
+              1日目11回、3日目9回の全20回上映を予定しています。
+              <br>
+              <br>
+              また今年の投影は<span style="font-size:1.5em;">予約が必須</span> となります。<a href="https://miyakomatsuri.com" target="_blank">みやこ祭ホームページ</a>より予約フォームの入力をお願いします。
+
+              <ul class="kome">
+                <li>
+              機材のトラブルなどにより予定を変更する場合がございます。予めご了承ください。
+                </li>
+                <li>
+                  チケットが残っている回は会場にて当日予約も可能です。
+                </li>
+              </ul>
+              
+            </p> 
+
+
+
+            <table>
+            
+              <tr>
+                <td class="date">11.3(木)</td> <td>12:00-</td> <td>12:30-</td> <td>13:00-</td> <td>13:30-</td>
+              </tr>
+              
+              <tr>
+                <td class="non"></td> <td>14:00-</td> <td>15:00-</td> <td>15:30-</td> <td>16:00-</td> 
+              </tr>
+              <tr>
+                <td class="non"></td> <td>16:30-</td> <td>17:00-</td>
+              </tr>
+              <tr>
+                <td class="date">11.5(土)</td> <td>12:00-</td> <td>12:30-</td> <td>13:00-</td> <td>13:30-</td>
+              </tr>
+            
+              <tr>
+                <td class="non"></td> <td>14:00-</td> <td>15:00-</td><td>15:30-</td> <td>16:00-</td>
+              </tr>
+              <tr>
+                <td class="non"></td> <td>16:30-</td>
+              </tr>
+            
+            </table>
+
+          
+ 
+            <h1>Archive</h1>
+
+            <h3>今年度アーカイブ配信</h3>
+
+            <p>1日目の上映終了後公開予定です。
+              もうしばらくお待ちください。
+              <br>
+              PM公式アカウントはこちら
+            </p>
+
+            <a href="https://www.youtube.com/channel/UCiAXi4RQHF5BnHx-HcrDTcQ/featured"><img class="youtubech" src="images/PM_youtube.jpg"></a>
+
+            <h3>2021年度作品</h3>
+            <div class="pc">
+            <iframe width="580" height="315" src="https://www.youtube.com/embed/EbNPCo6zBqA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div class="sp">
+              <iframe width="330" height="180" src="https://www.youtube.com/embed/EbNPCo6zBqA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+
+
+          <ul class="listback">
+          
+          </ul>
+          <h1>
+            Credit
+          </h1>
+
+          <h3>
+            Group1
+          </h3>
+
+          <p>
+            
+          </p>
+
+          <h3>
+            Group2
+          </h3>
+
+          <p>
+
+          </p>
+
+          <h3>
+            Group3
+          </h3>
+          <ul>
+            <li></li>
+            <li>Noguchi Saki</li>
+            <li>Tago Yasuko</li>
+            <li>Katayama Akari</li>
+            <li>Fukuda Haruka</li>
+            <li>Ichikawa Yuga</li>
+          </ul>
+
+          <h3>
+            Group4
+          </h3>
+
+          <ul>
+            <li>Yano Haruka</li>
+            <li>Noguchi Saki</li>
+            <li>Tago Yasuko</li>
+            <li>Katayama Akari</li>
+            <li>Fukuda Haruka</li>
+            <li>Ichikawa Yuga</li>
+          </ul>
+
+          <p class="end">
+            ©️ 2022 PM2022製作チーム
+          </p>
+
+        </div>
+
+          
+          
+          <script src="script.js" defer></script>
+    </body>
+    
+    
+
+</html>
